c4f7b5004685ae33c5e95830aa3b4bac53b16b19
Bionca Bond <bioncab@zillowgroup.com>
2021-12-08 19:33:08 -0800

    all

--- math-series/tests/test_series.py
+++ math-series/tests/test_series.py
@@ -1,27 +1,6 @@
 import pytest 
 from math_series.series import fibonacci,lucas, sum_series
 
-# “”” 
-# (<-Called doc string. Have a good description of what the function is doing)
-# This will be math series.
-# If number is tested/passed return the integer of the fibonacci sequence 
-# If number is tested/passed return the integer of the lucas sequence 
-# If the the function is called with no optional numbers, the first two numbers are a 0 and 1 return the integer of the fibonacci sequence 
-# If the the function is called with optional numbers,the optional two numbers are a 2 and 1 return the integer of the lucas sequence 
-# fibonacci(1) -> 1
-# fibonacci(7) -> 13
-# lucas(1) -> 2
-# lucas(8) -> 29
-# sum_series(8,2,1) -> 29 (lucas number since optional params 2,1)
-# sum_series(2,0,1) -> 1 (fibonacci number since optional params 0,1)
-# “””
-
-
-
-
-
-
-
 def test_one(): 
         actual = fibonacci(1) 
         expected = 1 
@@ -71,6 +50,7 @@ def test_three_sum_series():
         actual = sum_series(8,2,1)
         expected = 47
         assert actual == expected  
+        
 def test_none_sum_series():
         actual = sum_series(5)
         expected = 5
