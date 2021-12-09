1daef90a2c12299bb334f9130ac83426c50f1245
Bionca Bond <bioncab@zillowgroup.com>
2021-12-08 19:29:36 -0800

    sum_series

--- math-series/math_series/series.py
+++ math-series/math_series/series.py
@@ -1,5 +1,11 @@
 def fibonacci(num): 
-    ##base value
+    """
+    This will be fibonacci.
+    If number is tested/passed return the integer of the fibonacci sequence 
+    If the the function is called with no optional numbers, the first two numbers are a 0 and 1 return the integer of the fibonacci sequence 
+    fibonacci(1) -> 1
+    fibonacci(7) -> 13
+    """
     if num < 0: 
         print("Incorrect Entry.Be more positive.")
     elif num == 0:
@@ -9,8 +15,14 @@ def fibonacci(num):
     else:
         return fibonacci(num-1) + fibonacci(num-2)
 
-
 def lucas(num):
+    """
+    This will be lucas.
+    If number is tested/passed return the integer of the lucas sequence
+    If the the function is called with optional numbers,the optional two numbers are a 2 and 1 return the integer of the lucas sequence 
+    lucas(1) -> 2
+    lucas(8) -> 29
+    """
     if num <= 0:
         print("Incorrect Entry.Be more positive.")
     if num == 1:
@@ -21,11 +33,17 @@ def lucas(num):
         return lucas(num-1) + lucas(num-2)
 
 def sum_series(num, num1=0, num2=1):
+    """
+    This will be sum_series.
+    sum_series(8,2,1) -> 29 (lucas number since optional params 2,1)
+    sum_series(2,0,1) -> 1 (fibonacci number since optional params 0,1)
+    sum_series(5) -> 5 (since no optional parameters were entered defaulted to 0,1 as the optional::fibonacci)
+    """
   if num == 0:
     return num1
   if num == 1:
     return num2
   else:
-    return sum_series(num-1, num1, num2) + (sum_series(num-2, num1, n2))
+    return (sum_series(num-1, num1, num2)) + (sum_series(num-2, num1, num2))
     
 
--- math-series/tests/test_series.py
+++ math-series/tests/test_series.py
@@ -1,5 +1,25 @@
 import pytest 
-from math_series.series import fibonacci,lucas
+from math_series.series import fibonacci,lucas, sum_series
+
+# “”” 
+# (<-Called doc string. Have a good description of what the function is doing)
+# This will be math series.
+# If number is tested/passed return the integer of the fibonacci sequence 
+# If number is tested/passed return the integer of the lucas sequence 
+# If the the function is called with no optional numbers, the first two numbers are a 0 and 1 return the integer of the fibonacci sequence 
+# If the the function is called with optional numbers,the optional two numbers are a 2 and 1 return the integer of the lucas sequence 
+# fibonacci(1) -> 1
+# fibonacci(7) -> 13
+# lucas(1) -> 2
+# lucas(8) -> 29
+# sum_series(8,2,1) -> 29 (lucas number since optional params 2,1)
+# sum_series(2,0,1) -> 1 (fibonacci number since optional params 0,1)
+# “””
+
+
+
+
+
 
 
 def test_one(): 
@@ -49,7 +69,11 @@ def test_two_sum_series():
 
 def test_three_sum_series():
         actual = sum_series(8,2,1)
-        expected = 29
+        expected = 47
+        assert actual == expected  
+def test_none_sum_series():
+        actual = sum_series(5)
+        expected = 5
         assert actual == expected  
 
 
